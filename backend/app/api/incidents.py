fa2110db867184bd8e4fec81cc0630b9f9bdbfb8
Demend2607 <finder2607011@gmail.com>
2026-03-03 12:12:51 +1000

    fetch upgrade | incidents API - in progress

--- backend/app/api/incidents.py
+++ backend/app/api/incidents.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, Query
+from datetime import datetime
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, extract
 
@@ -16,12 +17,30 @@ async def get_db_session():
 @router.get("/")
 async def get_incidents(
     db: AsyncSession = Depends(get_db_session),
-    day: int | None = Query(None, description="day"),
+    month: int | None = Query(None, description="Month", ge=1, le=12),
 ):
     query = select(Incident).order_by(Incident.date_ts.desc())
 
-    if day:
-        query = query.where(extract("day", Incident.date_ts) == day)
+    if month:
+        query = query.where(extract("month", Incident.date_ts) == month)
 
     result = await db.execute(query)
     return result.scalars().all()
+
+
+@router.get("/{day}/")
+async def get_incident(
+    # id: int,
+    day: int,
+    db: AsyncSession = Depends(get_db_session),
+):
+    # "date_raw": "2026-03-02T15:15:00+10:00",
+    # "date_ts": "2026-03-02T15:15:00+10:00",
+    query = select(Incident).where(Incident.date_ts == day)
+    result = await db.execute(query)
+    incident = result.scalars().one_or_none()
+
+    if not incident:
+        raise HTTPException(status_code=404, detail="Incident not found")
+
+    return incident
--- backend/app/services/incidents/fetch.py
+++ backend/app/services/incidents/fetch.py
@@ -3,23 +3,20 @@ from typing import Any
 import requests
 
 
-COOKIES = {
-    '_ga': 'GA1.1.1446923521.1769143658',
-    '_ga_J9N3D9YFK9': 'GS2.1.s1770271791$o8$g1$t1770272584$j29$l0$h0$dmPA7HMywgvDN5PaPPs0wpKFkNQPTVgLMUw',
-}
-
-HEADERS = {
+HTTPS_HEAD = {
     'accept': 'application/json, text/plain, */*',
-    'accept-language': 'ru-RU,ru;q=0.7',
     'referer': 'https://www.dvnovosti.ru/incidents/',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
-    # 'cookie': '_ga=GA1.1.1446923521.1769143658; _ga_J9N3D9YFK9=GS2.1.s1770271791$o8$g1$t1770272584$j29$l0$h0$dmPA7HMywgvDN5PaPPs0wpKFkNQPTVgLMUw',
 }
 
 
 def fetch_incidents(url) -> dict[str, Any]:
     with requests.Session() as session:
-        response = session.get(url, headers=HEADERS,
-                               cookies=COOKIES, timeout=20)
+        session.headers.update({
+            "accept": HTTPS_HEAD['accept'],
+            "referer": HTTPS_HEAD['referer'],
+            "user-agent": HTTPS_HEAD['user-agent'],
+        })
+        response = session.get(url, timeout=20)
         response.raise_for_status()
         return response.json()
